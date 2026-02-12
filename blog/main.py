349082aa4379b43db9c715ca38c72057f3bdd16b
aakaaashmann <aks5060708090100@gmail.com>
2026-02-12 14:30:56 +0530

    learned about response code and HTTPExceptions

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Depends
+from fastapi import FastAPI , Depends , status , Response, HTTPException
 from . import schemas , models
 from .database import engine , SessionLocal
 from sqlalchemy.orm import Session
@@ -14,8 +14,8 @@ def get_db():
         yield db
     finally:
         db.close()
-# creating new blog
-@app.post("/blog")
+# creating new blog and set the status code
+@app.post("/blog", status_code = status.HTTP_201_CREATED) 
 def create(request : schemas.Blog,db : Session = Depends(get_db)): #created an instance for db
     new_blog = models.Blog(title=request.title,body=request.body)
     db.add(new_blog)
@@ -30,9 +30,13 @@ def all(db : Session = Depends(get_db)):
     return blogs
 
 #show the particular blog with particular id
-@app.get("/blog/{id}")
-def temp(id,db : Session = Depends(get_db)):
+@app.get("/blog/{id}",status_code = 200)
+def temp(id, response : Response ,db : Session = Depends(get_db)):
     #this is a filter (or say where) in sql and we are getting the fist blog not all the blogs
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first() 
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with the id {id} is not available")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"detail":f"Blog with the id {id} is not available"}
     return blog
 
