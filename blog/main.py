ee8716a2ea2b0e80daaddd8f9ad477786e271308
aakaaashmann <aks5060708090100@gmail.com>
2026-02-12 13:59:55 +0530

    stored blogs to using post method, get all blogs and a particular blog with id using get method

--- blog/main.py
+++ blog/main.py
@@ -14,21 +14,25 @@ def get_db():
         yield db
     finally:
         db.close()
-
+# creating new blog
 @app.post("/blog")
-def create(request : schemas.Blog,db : Session = Depends(get_db)):
+def create(request : schemas.Blog,db : Session = Depends(get_db)): #created an instance for db
     new_blog = models.Blog(title=request.title,body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
-
+#to fetch all the blogs
 @app.get("/blog")
 def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog")
-def temp():
-    pass
+#show the particular blog with particular id
+@app.get("/blog/{id}")
+def temp(id,db : Session = Depends(get_db)):
+    #this is a filter (or say where) in sql and we are getting the fist blog not all the blogs
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first() 
+    return blog
+
