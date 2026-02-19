de63ecb612b518dec93f8e09bd97b9f47cd39780
aakaaashmann <aks5060708090100@gmail.com>
2026-02-19 20:24:07 +0530

    learned about API router operations and blog and user func repositories flow of code and api routes

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -3,58 +3,38 @@ from fastapi import APIRouter , Depends, status, Response, HTTPException
 
 from .. import schemas , database, models
 from sqlalchemy.orm import Session
+from ..repository import blog
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/blog",
+    tags = ["Blogs"]
+)
 
 get_db = database.get_db
 
 
-
-
-@router.get("/blog", response_model = List[schemas.ShowBlog], tags = ["blogs"])
+@router.get("/", response_model = List[schemas.ShowBlog])
 def all(db : Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    return blog.get_all(db)
 
 
 # creating new blog and set the status code
-@router.post("/blog", status_code = status.HTTP_201_CREATED, response_model=schemas.ShowBlog, tags = ["blogs"]) 
+@router.post("/", status_code = status.HTTP_201_CREATED, response_model=schemas.ShowBlog) 
 def create(request : schemas.Blog,db : Session = Depends(get_db)): #created an instance for db
-    new_blog = models.Blog(title=request.title,body=request.body,user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(request,db)
 
 #deleting a particular blog where id == particular id
-@router.delete("/blog/{id}", status_code= status.HTTP_204_NO_CONTENT, tags = ["blogs"])
+@router.delete("/{id}", status_code= status.HTTP_204_NO_CONTENT)
 def destroy(id,db:Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return 
+    return blog.destroy(id,db) 
 
 #update a particular blog passing the request body schema of blog
-@router.put("/blog/{id}",status_code = status.HTTP_202_ACCEPTED, tags = ["blogs"])
+@router.put("/{id}",status_code = status.HTTP_202_ACCEPTED)
 def update(id,request:schemas.Blog,db:Session = Depends(get_db)):
-    blog =db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
-    blog.update(request.dict())
-    db.commit()
-    return  "updated successfully"
+    return blog.update(id,request,db)
            
 
-
 #show the particular blog with particular id
-@router.get("/blog/{id}",status_code = status.HTTP_200_OK, response_model=schemas.ShowBlog, tags = ["blogs"])
-def temp(id,db : Session = Depends(get_db)):
-    #this is a filter (or say where) in sql and we are getting the fist blog not all the blogs
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with the id {id} is not available")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"detail":f"Blog with the id {id} is not available"}
-    return blog
+@router.get("/{id}",status_code = status.HTTP_200_OK, response_model=schemas.ShowBlog)
+def show(id,db : Session = Depends(get_db)):
+    return blog.show(id,db)
